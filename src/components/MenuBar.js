0e1269e2515e040cf75efe2249ab6c13d8fab5f9
melonybox <grandmadcow@gmail.com>
2019-06-10 11:17:25 -0400

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -4,7 +4,7 @@ const MenuBar = (props) => {
 
   /*
 
-  The 'a' tags below are the menu items. Think about the way a menu 
+  The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
@@ -27,12 +27,11 @@ const MenuBar = (props) => {
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a className="item" id="pokemon">
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
-
 }
 
 export default MenuBar
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,6 +4,43 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  state = {
+    activeButton: Profile
+  }
+
+  handActiveState = (event) => {
+    if ((event.target.classList.contains("icon") && (event.target.parentElement.classList.value === "item")) || (event.target.classList.contains("item") && (event.target.parentElement.classList.value === "ui four item menu"))){
+      const name = event.target.id
+      const currActive = document.getElementsByClassName("active")
+      currActive[0].classList.remove("active")
+
+      if (name === "photo") {
+        this.setState({
+          activeButton: Photos
+        })
+        document.getElementsByTagName("A").photo.classList.add("active")
+      } else if (name === "cocktail") {
+        this.setState({
+          activeButton: Cocktails
+        })
+        document.getElementsByTagName("A").cocktail.classList.add("active")
+
+      } else if (name === "pokemon") {
+        this.setState({
+          activeButton: Pokemon
+        })
+        document.getElementsByTagName("A").pokemon.classList.add("active")
+
+      } else if (name === "profile") {
+        this.setState({
+          activeButton: Profile
+        })
+        document.getElementsByTagName("A").profile.classList.add("active")
+
+      }
+    }
+  }
+
 
   render() {
 
@@ -16,9 +53,9 @@ class MainBox extends React.Component {
     const detailsToDisplay = <div>Hi, I'm a div!</div>
 
     return (
-      <div>
+      <div onClick={this.handActiveState}>
         <MenuBar />
-        {detailsToDisplay}
+        <this.state.activeButton />
       </div>
     )
   }
